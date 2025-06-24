c84ca84802deb79d209de889c38b7c47e72b2856
KsaVierCoder <ksavier06@interia.pl>
2025-06-24 10:33:37 +0200

    Ufo

---
+++ Main.py
@@ -0,0 +1,11 @@
+# === IMPORTY ===
+from tkinter import *
+from tkinter import ttk
+import tkintermapview
+import requests
+from bs4 import BeautifulSoup
+
+# === LISTY ===
+sieci_dronow = []
+operatorzy = []
+klienci = []
