18d4ed2ae948d71b47256240cec0526b82f5db54
KsaVierCoder <ksavier06@interia.pl>
2025-06-24 10:41:29 +0200

    Funkcje mapy

--- Main.py
+++ Main.py
@@ -41,3 +41,39 @@ class Klient:
         self.wspolrzedne = siec.wspolrzedne
         self.marker = map_widget.set_marker(self.wspolrzedne[0], self.wspolrzedne[1], text=f"Klient: {self.imie}")
 
+# === FUNKCJE MAPY ===
+def pokaz_wszystkie_sieci():
+    map_widget.set_position(52.23, 21.0)
+    map_widget.set_zoom(6)
+    for o in operatorzy: o.marker.delete()
+    for k in klienci: k.marker.delete()
+    for s in sieci_dronow:
+        s.marker = map_widget.set_marker(s.wspolrzedne[0], s.wspolrzedne[1], text=f"Sieć: {s.nazwa}")
+
+def pokaz_wszystkich_operatorow():
+    for s in sieci_dronow: s.marker.delete()
+    for k in klienci: k.marker.delete()
+    for o in operatorzy:
+        o.marker = map_widget.set_marker(o.wspolrzedne[0], o.wspolrzedne[1], text=f"Operator: {o.imie} {o.nazwisko}")
+
+def pokaz_klientow_sieci():
+    for s in sieci_dronow: s.marker.delete()
+    for o in operatorzy: o.marker.delete()
+    for k in klienci: k.marker.delete()
+    idx = combobox_siec_na_mapie.current()
+    if idx >= 0:
+        siec = sieci_dronow[idx]
+        for k in klienci:
+            if k.siec == siec:
+                k.marker = map_widget.set_marker(k.wspolrzedne[0], k.wspolrzedne[1], text=f"Klient: {k.imie}")
+
+def pokaz_operatorow_sieci():
+    for s in sieci_dronow: s.marker.delete()
+    for o in operatorzy: o.marker.delete()
+    for k in klienci: k.marker.delete()
+    idx = combobox_siec_na_mapie.current()
+    if idx >= 0:
+        siec = sieci_dronow[idx]
+        for o in operatorzy:
+            if o.siec == siec:
+                o.marker = map_widget.set_marker(o.wspolrzedne[0], o.wspolrzedne[1], text=f"Operator: {o.imie} {o.nazwisko}")
